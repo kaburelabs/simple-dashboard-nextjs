a9df1471f8e5c64202316c9a16347f26a09ef142
Leonardo Ferreira <ferreira_master@hotmail.com>
2024-01-18 15:18:44 -0300

    fixing a problem in the lusitana font

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,10 +1,10 @@
-import { lusi } from '@/app/ui/fonts';
+import { lusitana } from '@/app/ui/fonts';
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
 
 export default function AcmeLogo() {
   return (
     <div
-      className={`${lusi.className} flex flex-row items-center leading-none text-white`}
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="mr-[8px] h-12 w-12 rotate-[15deg] md:mr-[16px]" />
       <p className="text-[32px]">Global Pharma</p>
